ff408c834fb4a1d990d27516e75c5070a2367115
arkanael <arkanael@gmail.com>
2021-04-25 16:51:02 -0300

    Listando extrato

--- src/index.js
+++ src/index.js
@@ -9,14 +9,25 @@ app.use(express.json());
 
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body;
-    const id = uuidv4();
-    customers.push({
-        cpf, name, id, statement: []
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+    if(customerAlreadyExists){
+        return response.status(400).json({error: "Customer already existis!"});
+    }
 
+    const id = uuidv4();
+
+    customers.push({
+        cpf, name, id: uuidv4(), statement: []
 
     });
 
     return response.status(201).send();
+});
+
+app.get('/statement/:cpf', (request, response) => {
+    const {cpf} = request.params;
+    const customer = customers.find(customer => customer.cpf === cpf);
+    return response.json(customer.statement);
 });
 
 app.listen('3333');
