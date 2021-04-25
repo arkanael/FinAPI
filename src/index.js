c6791e72f378f347c77898cfa67010f80a5fc4c1
arkanael <arkanael@gmail.com>
2021-04-25 16:33:56 -0300

    Cadastro de contas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {v4: uuidv4} = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
@@ -7,13 +7,15 @@ const customers = [];
 
 app.use(express.json());
 
-app.post('/account', (request, response)=>{
-    const {cpf, name} = request.body;
+app.post('/account', (request, response) => {
+    const { cpf, name } = request.body;
     const id = uuidv4();
     customers.push({
-        cpf, name, id, statement:[]
+        cpf, name, id, statement: []
+
+
     });
-    
+
     return response.status(201).send();
 });
 
