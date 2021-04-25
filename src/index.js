3886b6d039fc246286aa9d6fffd8e6b266c7979e
arkanael <arkanael@gmail.com>
2021-04-25 11:06:24 -0300

    Criando o POST

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const {v4: uuidv4} = require('uuid');
 
 const app = express();
 
-app.get('/', (request, response)=>{
-    return response.json();
+const customers = [];
+
+app.use(express.json());
+
+app.post('/account', (request, response)=>{
+    const {cpf, name} = request.body;
+    const id = uuidv4();
+    customers.push({
+        cpf, name, id, statement:[]
+    });
+    
+    return response.status(201).send();
 });
 
 app.listen('3333');
